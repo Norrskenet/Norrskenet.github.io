03d84533a793d97e83cc8d3b1938cb8060bf99d1
unknown <andersparr@gmail.com>
2019-10-01 11:58:26 +0200

    Started the project added a main layout and a home page, plus started writing abit of js code

---
+++ test/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.engine("hbs", expressHandlebars({
+	defaultLayout: "main.hbs"
+}))
+
+app.get("/", function(request,response){
+    response.render("home.hbs")
+})
+
+app.get("/", function(request,response){
+    response.render("about.hbs")
+})
+
+app.listen(8080)
